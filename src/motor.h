3e6dcee3c36e44b69624daa2f2104d16b4ad8567
DavidHearn <david.c.hearn@outlook.com>
2023-08-22 17:19:37 -0700

    Added the ability to control the attenuation of motors over the network
    - Moved the network stuff out of main
    - Added a way to differentiate between packets for motor strength control and those for attenuation control.
    
    god help me when I un-spaghettify this

--- src/motor.h
+++ src/motor.h
@@ -4,6 +4,7 @@
 #include <config.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <packet.h>
 
 // Takes a motorID assigned in the PWM_PINS array in config.h.
 // Takes a Strength value from 0-255.
@@ -14,5 +15,9 @@ void WriteToMotor(size_t motorID, uint8_t Str);
 
 // Updates the attenuation values of motors when it is called
 void CheckStrAttenuation();
+
+void UpdateMotorStrength(uint8_t *StrArray, size_t length);
+
+void UpdateAttenuationFunc(struct Attenuation_Control control);
 
 #endif
