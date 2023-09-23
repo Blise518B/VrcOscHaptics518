75bd6084cbf1e70e1f755b0fe48da434f63cc36a
Sky Hearn <david.c.hearn@outlook.com>
2023-09-23 14:27:29 -0700

    Fix dumb bugs

--- src/config.h
+++ src/config.h
@@ -11,6 +11,6 @@ const int WEB_SOCKET_PORT = 8080;
 // Motor Configuration
 const int PWM_PINS[] = {D1, D2, D5, D6, D7, D8};
 const size_t NUM_PINS = sizeof(PWM_PINS) / sizeof(PWM_PINS[0]);
-const unsigned long ATTENUATION_TIME = 10000;
+const uint FALLOFF_REFRESH_RATE = 5; // how often, in HZ, should we update the falloff of haptic sensors. 
 
 #endif
